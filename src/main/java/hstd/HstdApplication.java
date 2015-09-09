ac7913450a5de063411c3420dc0991fd7d06e001
gg_trandtb <trandtb@greenglobal.vn>
2015-09-09 09:44:16 +0700

    Init Project

---
+++ src/main/java/hstd/HstdApplication.java
@@ -0,0 +1,12 @@
+package hstd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HstdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HstdApplication.class, args);
+    }
+}
---
+++ src/test/java/hstd/HstdApplicationTests.java
@@ -0,0 +1,18 @@
+package hstd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HstdApplication.class)
+@WebAppConfiguration
+public class HstdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
